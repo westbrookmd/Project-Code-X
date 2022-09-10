87a321a0ee1bacbb92cc9c74698c1f9620adcb6f
westbrookmd <westbrookmd1@gmail.com>
2022-09-10 12:41:06 -0400

    Add project files.

---
+++ ProjectCodeX/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectCodeX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
