98db4ca7ad572bad080f51591a350dae94deb56f
westbrookmd <westbrookmd1@gmail.com>
2022-09-13 18:12:37 -0400

    Initial settings changes

--- ProjectCodeX/Program.cs
+++ ProjectCodeX/Program.cs
@@ -11,7 +11,7 @@ namespace ProjectCodeX
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection") ?? throw new InvalidOperationException("Connection string 'DatabaseConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
