ecaef6641c200eeca987516274c732a9c172445a
MichaelCompton21 <113479203+MichaelCompton21@users.noreply.github.com>
2022-09-27 17:52:07 -0400

    Update users_t.sql

--- ProjectCodeXDatabase/Tables/users_t.sql
+++ ProjectCodeXDatabase/Tables/users_t.sql
@@ -2,5 +2,8 @@
 (
 	UserID		varchar(10) Not Null Primary Key,
 	FirstName	varchar(20) Null,
-	LastName	varchar(20) Null
+	LastName	varchar(20) Null,
+	Address 	varchar(100) Null,
+	City		varchar(25) Null,
+	State		varchar(2) Null
 )
