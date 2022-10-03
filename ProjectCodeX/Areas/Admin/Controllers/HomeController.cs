ecf4dd564d8535e4caa21b472fec7f35ea0fecbe
gendarmeofficer <firstandlast11152@yahoo.com>
2022-10-03 14:58:42 -0400

    Admin Area & News Home
    Added new Admin Area, includes basic controllers and views for Event, News, and Home pages. Added new view to News to hold info about articles. Index updated to show article titles, images, and dates, placeholder data inserted.

---
+++ ProjectCodeX/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectCodeX.Models;
+using System.Diagnostics;
+
+namespace ProjectCodeX.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            ErrorViewModel error = new ErrorViewModel 
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier 
+            };
+            _logger.LogError($"Error with RequestId:{error.RequestId}", error);
+
+            return View();
+        }
+    }
+}
