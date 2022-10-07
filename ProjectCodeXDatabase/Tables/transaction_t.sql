10f056b39fe841fe9bc5543721b603365b85e1b2
westbrookmd <westbrookmd1@gmail.com>
2022-10-07 18:47:25 -0400

    Identity Password Configuration
    Additionally fixes the transaction table to prevent build errors

--- ProjectCodeXDatabase/Tables/transaction_t.sql
+++ ProjectCodeXDatabase/Tables/transaction_t.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [dbo].[transaction_t]
 (
 	TransID 	int Not Null primary key,
-	UserID    int Not Null foreign key,
-  TransType varchar(20) Null
+	UserID		int Not Null,
+	TransType	varchar(20) Null,
 	Amount 		smallmoney Null,
 	Notes 		varchar(500) Null
 )
