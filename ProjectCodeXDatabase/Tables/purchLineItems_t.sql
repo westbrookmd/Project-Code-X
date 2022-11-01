8eaf9e81f8be076a393fa212e792ba3dc4365190
MichaelCompton21 <113479203+MichaelCompton21@users.noreply.github.com>
2022-11-01 17:31:23 -0400

    Update purchLineItems_t.sql

--- ProjectCodeXDatabase/Tables/purchLineItems_t.sql
+++ ProjectCodeXDatabase/Tables/purchLineItems_t.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [dbo].[purchaseLineItems_t]
 (
-  PLineID 	int Not Null primary key,
+  	PLineID 	int Not Null primary key,
 	PurchID 	int Not Null foreign key,
-  	InventoryID 		int Not Null foreign key,
+  	InventoryID 	int Not Null foreign key,
   	Quantity   	int Null,
 	Price   	smallmoney Null
 )
