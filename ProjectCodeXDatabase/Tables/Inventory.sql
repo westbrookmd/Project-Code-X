b3e037656f52cfaaa6acbaf911cda40b2a99cb21
MichaelCompton21 <113479203+MichaelCompton21@users.noreply.github.com>
2022-11-15 19:52:48 -0500

    Update Inventory.sql

--- ProjectCodeXDatabase/Tables/Inventory.sql
+++ ProjectCodeXDatabase/Tables/Inventory.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[Inventory] (
-    [InvID] INT        IDENTITY (1, 1) NOT NULL,
-    [ItemName] VARCHAR(30)        NULL,
-    [Qnty]    INT        NULL,
+    [InvID]         INT        IDENTITY (1, 1) NOT NULL,
+    [ItemName]      VARCHAR(30) NULL,
+    [Qnty]          INT        NULL,
     [Description]   VARCHAR(100) NULL,
     PRIMARY KEY CLUSTERED ([InvID] ASC)
 );
