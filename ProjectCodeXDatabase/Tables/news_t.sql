014c06baca2de755ec471bb58f9025c919eecbcb
MichaelCompton21 <113479203+MichaelCompton21@users.noreply.github.com>
2022-11-15 19:36:05 -0500

    Update news_t.sql

--- ProjectCodeXDatabase/Tables/news_t.sql
+++ ProjectCodeXDatabase/Tables/news_t.sql
@@ -2,8 +2,7 @@ CREATE TABLE [dbo].[news_t]
 (
 	ArticleID 	INT           IDENTITY (1, 1) NOT NULL,
 	PublishDate 	date Null,
-	Type 		varchar(30) Null,
-	Link 		varchar(100) Null,
+	Summary 	varchar(30) Null,
 	ViewCount 	int Null,
 	Author 		varchar(40) Null
 	PRIMARY KEY CLUSTERED ([ArticleID] ASC)
