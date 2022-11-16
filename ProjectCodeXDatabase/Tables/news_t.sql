655dc177362005d6ca08ed705da631b0901cd2b1
MichaelCompton21 <113479203+MichaelCompton21@users.noreply.github.com>
2022-11-15 19:30:56 -0500

    Update news_t.sql

--- ProjectCodeXDatabase/Tables/news_t.sql
+++ ProjectCodeXDatabase/Tables/news_t.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[news_t]
 (
-	ArticleID 	int Not Null primary key,
+	ArticleID 	INT           IDENTITY (1, 1) NOT NULL,
 	PublishDate 	date Null,
 	Type 		varchar(30) Null,
 	Link 		varchar(100) Null,
